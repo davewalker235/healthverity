55f2ac6b80135732a4554fce28a34425feca2fd7
David Walker <david.walker@health-union.com>
2019-08-29 11:57:57 -0400

    For some reason this damn counter logic is driving me nuts

--- question3.py
+++ question3.py
@@ -2,34 +2,55 @@ import argparse
 
 def main(filename, part):
   """
-  Compare two files of newline separated strings and return the number of
-  strings both files share.
+  Compare two csv files, --part a returns the number missing from column A,
+  --part b returns the number missing from both
 
   lista.txt is missing 75, Column A is missing 82
+  listb.txt is missing 68, Column A is missing 59
 
     $ question3.py --part a --file question3/lista.txt
       82
 
   """
   with open(filename, 'r') as file:
-    properA = 0
-    properB = 0
+    # We're going to keep a counter for each column to track what we expect
+    # the integer to be and compare it with the values. This should let us
+    # identify the mismatches without much overhead
+    properA = 1
+    properB = 1
     for line in file:
-      properA += 1
-      properB += 1
       [a, b] = line.strip().split(',')
 
-      if str(properA) != a and str(properB) != b:
-        if part == 'b':
-          return properA
-        else:
-          properA += 1
-          properB += 1
+      missingA = str(properA) != a
+      missingB = str(properB) != b
 
-      if str(properA) != a and str(properB) == b:
-        properA += 1
-        if part == 'a':
-          return b
+      print(a, properA, b, properB, missingA, missingB)
+
+      if part == 'a' and missingA and not missingB:
+        return properB
+
+      if part == 'b' and missingA and missingB:
+        return properA
+
+      if not missingA: properA += 1
+      if not missingB: properB += 1
+
+
+
+      # if str(properA) != a and str(properB) != b:
+      #   print('parta', a, b, properA, properB)
+      #   if part == 'b':
+      #     return properA
+      #   else:
+      #     properA += 1
+      #     properB += 1
+      #
+      # if str(properA) != a and str(properB) == b:
+      #   print('partb', a, b, properA, properB)
+      #
+      #   properA += 1
+      #   if part == 'a':
+      #     return properB
 
 # Switch to argparse for a nicer interface, I've forgotten a lot of this stuff
 parser = argparse.ArgumentParser()
